4fe5a009408a0617ff93de70c14919154edac462
Carlos Larico <Carlos Larico>
2016-02-01 10:37:12 -0500

    add output

--- src/Main.java
+++ src/Main.java
@@ -17,23 +17,24 @@ public class Main {
 		ent.setDate_mutante(date1);
 		date1.setMonth(3);
 		
-		System.out.println("d1 "+date1.toLocaleString());
-		System.out.println("d1 "+ ent.getDate_mutante().toLocaleString());
+		System.out.println("d1 "+date1.toLocaleString()); //output -> d1 01/04/2016 10:30:39 AM
+		System.out.println("d1 "+ ent.getDate_mutante().toLocaleString()); // output -> d1 01/04/2016 10:30:39 AM
 		
 		// para la fecha no mutante
 		Date date2 =  new Date();
 		ent.setDate_no_mutante(date2);
 		date2.setMonth(4);
 		
-		System.out.println("d2 "+date2.toLocaleString());
-		System.out.println("d2 "+ ent.getDate_no_mutante().toLocaleString());
+		System.out.println("d2 "+date2.toLocaleString()); //output -> d2 01/05/2016 10:30:39 AM
+		System.out.println("d2 "+ ent.getDate_no_mutante().toLocaleString());//output --> d2 01/02/2016 10:30:39 AM
 		
 		String cadena = "Eduardo";
 		ent.setCadena(cadena);
 		cadena = "Pirulo";
 		
-		System.out.println("c2 "+cadena);
-		System.out.println("c2 "+ent.getCadena());
+		System.out.println("c2 "+cadena);//output -> c2 Pirulo
+		System.out.println("c2 "+ent.getCadena());//output --> c2 Eduardo
+
 		
 	}
 }
